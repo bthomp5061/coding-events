bb3b66b40bd70dac55e5ca1c9b2db47b61c090d4
bthomp5061 <85450715+bthomp5061@users.noreply.github.com>
2021-11-29 19:43:50 -0500

    added nav for tags

--- src/main/java/org/launchcode/codingevents/controllers/EventCategoryController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventCategoryController.java
@@ -13,9 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.validation.Valid;
 
-/**
- * Created by Chris Bay
- */
+
 @Controller
 @RequestMapping("eventCategories")
 public class EventCategoryController {
